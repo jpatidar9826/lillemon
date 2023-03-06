8b159040b63487732532f36ceb28a03bb57a719a
jpatidar9826 <jpatidar9826@gmail.com>
2023-03-06 15:21:02 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Routing from "./components/Routing";
+import Footer from "./components/Footer";
 function App() {
   return (
-    <div className="App">home</div>
+    <>
+      <Navbar />
+      <Routing />
+      <Footer />
+    </>
   );
 }
 
